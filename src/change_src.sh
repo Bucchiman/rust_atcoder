eacbf5ad92392d395e3a5dd1650ac23b7e131285
Bucchiman <iwabuchi@fun.bio.keio.ac.jp>
2022-07-09 09:47:32 +0900

    code extract fixed

--- src/change_src.sh
+++ src/change_src.sh
@@ -2,7 +2,7 @@
 #
 # FileName: 	change_src
 # CreatedDate:  2022-06-07 23:28:02 +0900
-# LastModified: 2022-06-08 16:40:00 +0900
+# LastModified: 2022-07-09 09:46:41 +0900
 #
 
 
@@ -16,25 +16,30 @@ then
     rm tmp.rs
 fi
 
-while getopts :f: OPT
+while getopts :f:n: OPT
 do
     case $OPT in
         f) object_file=$OPTARG;;
+        n) name_func=$OPTARG;;
         :|\?) _usage;;
     esac
 done
 
 echo "use proconio::input;\n" > tmp.rs
-cat $object_file | awk -v flag=0 '{
-    if ($1 == "pub" || flag == 1){
+cat $object_file | awk -v flag=0 -v fn="${name_func}" '
+{
+if ($3 == fn || $3 == fn "()" || flag == 1){
         if (flag == 0){
-            flag = 1
-            printf "fn main() {\n"
+            flag = 1;
+            printf "fn main() {\n";
         }
         else{
-            print $0
+            print $0;
         }
     }
+if (flag == 1 && length($0) == 1 && $1 == "}"){
+    flag = 0;
+}
 }' >> tmp.rs
 
 return
