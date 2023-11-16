391b5514ef5c35d77c49bc276c563b51542a3702
Yuki Iwabuchi <8ucchiman@8ucchiman.local>
2023-11-16 23:17:41 +0900

    update 327

---
+++ src/abc_327.rs
@@ -0,0 +1,23 @@
+/*
+ * FileName:        abc_327
+ * Author:          8ucchiman
+ * CreatedDate:     2023-11-15 22:47:53
+ * LastModified:    2023-01-23 14:15:07 +0900
+ * Reference:       8ucchiman.jp
+ * Description:     ---
+ */
+
+
+use std::error::Error;
+
+
+pub fn abc_327_a() {
+
+}
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    
+    Ok(())
+}
+
